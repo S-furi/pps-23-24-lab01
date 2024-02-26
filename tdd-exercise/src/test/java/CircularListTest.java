8723043d07f019b9c106aa9a7032ce2220358f4f
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 11:19:00 +0100

    refactor: avoid repetition of list creation

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import tdd.CircularList;
@@ -13,28 +14,27 @@ import tdd.CircularList;
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    private final List<Object> elements = List.of(10, 20, 30);
+    private CircularList list ;
+
+    @BeforeEach
+    void setUp() {
+        this.list = new CircularListImpl(elements);
+    }
 
     @Test
     void testEmptyCircularListCreation() {
-        final CircularList list = new CircularListImpl(
-            Collections.emptyList()
-        );
+        this.list = new CircularListImpl(Collections.emptyList());
         assertTrue(list.isEmpty());
     }
 
     @Test
     void testNonEmptyCircularListCreation() {
-        final List<Object> elements = List.of(10, 20, 30);
-        final CircularList list = new CircularListImpl(elements);
-
         assertFalse(list.isEmpty());
     }
 
     @Test
     void testNonEmptyCircularListSize() {
-        final List<Object> elemets = List.of(10, 20, 30);
-        final CircularList list = new CircularListImpl(elemets);
-
-        assertEquals(elemets.size(), list.size());
+        assertEquals(this.elements.size(), list.size());
     }
 }
