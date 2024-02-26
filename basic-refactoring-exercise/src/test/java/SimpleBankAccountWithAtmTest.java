0fb2382c3aa34b2967d19ce11e0a81abd7d55a05
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 10:43:59 +0100

    feat: implement deposit test

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -1,15 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import example.model.AccountHolder;
 import example.model.BankAccount;
+import example.model.SimpleBankAccountWithAtm;
 
 public class SimpleBankAccountWithAtmTest {
     private BankAccount bankAccount;
     private AccountHolder accountHolder;
 
+    private static final double TEST_DEPOSIT_BALANCE = 100.0;
+
     @BeforeEach
     void setUp() {
         this.accountHolder = new AccountHolder("Stefano", "Furi", 0);
-        this.bankAccount = new SimpleBankAccountWithAtm(this.accountHolder);
+        this.bankAccount = new SimpleBankAccountWithAtm(this.accountHolder, 0.0);
     }
+
+    @Test
+    void testEmptyBankAccount() {
+        assertEquals(this.bankAccount.getBalance(), 0.0);
+    }
+
+    @Test
+    void testDeposit() {
+        this.bankAccount.deposit(this.accountHolder.getId(), TEST_DEPOSIT_BALANCE);
+        assertEquals(this.bankAccount.getBalance(), TEST_DEPOSIT_BALANCE - SimpleBankAccountWithAtm.ATM_FEE);
+    }
+
+    @Test
 }
