0842f2482ec5d7ad45068762d02ed40fed630eaa
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 10:29:31 +0100

    chore: autogenerate interface methods

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtm.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtm.java
@@ -2,9 +2,34 @@ import example.model.AccountHolder;
 import example.model.BankAccount;
 
 public class SimpleBankAccountWithAtm implements BankAccount {
+    private final AccountHolder accountHolder;
 
-    public SimpleBankAccountWithAtm(AccountHolder accountHolder) {
-        //TODO Auto-generated constructor stub
+    public SimpleBankAccountWithAtm(final AccountHolder accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHolder'");
+    }
+
+    @Override
+    public double getBalance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+    }
+
+    @Override
+    public void withdraw(int userID, double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
     }
 
 }
