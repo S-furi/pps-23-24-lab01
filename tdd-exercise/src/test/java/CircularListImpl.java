cd7b78d2afc12fb6b344ff92b353176a5d376a38
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 11:41:07 +0100

    refactor: make use of `listIterator` (supports previous)

--- tdd-exercise/src/test/java/CircularListImpl.java
+++ tdd-exercise/src/test/java/CircularListImpl.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Optional;
 
 import tdd.CircularList;
 
 public class CircularListImpl implements CircularList {
     private final List<Integer> list;
-    private Iterator<Integer> listIterator;
+    private ListIterator<Integer> listIterator;
 
     public CircularListImpl(final List<Integer> list) {
         this.list = new ArrayList<>(list);
-        this.listIterator = this.list.iterator();
+        this.listIterator = this.list.listIterator();
     }
 
     @Override
@@ -32,7 +33,7 @@ public class CircularListImpl implements CircularList {
     @Override
     public Optional<Integer> next() {
         if (!this.listIterator.hasNext()) {
-            this.listIterator = this.list.iterator();
+            this.listIterator = this.list.listIterator();
         }
         return Optional.of(this.listIterator.next());
     }
