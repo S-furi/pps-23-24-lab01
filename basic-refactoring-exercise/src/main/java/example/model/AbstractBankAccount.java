48456d784e6fb47e0f9ac239485026d7b796a165
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 10:42:56 +0100

    refactor: add abstract bank account

---
+++ basic-refactoring-exercise/src/main/java/example/model/AbstractBankAccount.java
@@ -0,0 +1,42 @@
+package example.model;
+public class AbstractBankAccount implements BankAccount {
+    private final AccountHolder accountHolder;
+    private double balance;
+
+    public AbstractBankAccount(final AccountHolder accountHolder, final double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        return this.accountHolder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.accountHolder.getId() == id;
+    }
+}
