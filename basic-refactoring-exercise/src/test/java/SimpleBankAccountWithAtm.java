ecdd2e8afba5ae37c893518b15716ec160830b0f
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 10:28:21 +0100

    feat: create `SimpleBankAccountWithAtm`

---
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtm.java
@@ -0,0 +1,10 @@
+import example.model.AccountHolder;
+import example.model.BankAccount;
+
+public class SimpleBankAccountWithAtm implements BankAccount {
+
+    public SimpleBankAccountWithAtm(AccountHolder accountHolder) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
