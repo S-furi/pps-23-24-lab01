ed35d16f8b5d3e6e103404929cb048328506c27e
S-furi <stefano.furi@studio.unibo.it>
2024-02-26 10:43:31 +0100

    feat: add `SimpleBankAccountWithAtm`

---
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,19 @@
+package example.model;
+
+public class SimpleBankAccountWithAtm extends AbstractBankAccount {
+    public static final double ATM_FEE = 1.0;
+
+    public SimpleBankAccountWithAtm(final AccountHolder accountHolder, final double balance) {
+        super(accountHolder, balance);
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        super.deposit(userID, amount - ATM_FEE);
+    }
+
+    @Override
+    public void withdraw(final int userID, double amount) {
+        super.withdraw(userID, amount - ATM_FEE);
+    }
+}
