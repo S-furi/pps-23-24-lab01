6ee3b17ab2be436c994f3b9e14f6266f3b83c690
S-furi <stefano.furi@studio.unibo.it>
2024-02-27 12:45:58 +0100

    chore: refactor tests

--- tdd-exercise/src/test/java/CircularListIteratorTest.java
+++ tdd-exercise/src/test/java/CircularListIteratorTest.java
@@ -1,26 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import tdd.iterator.CircularListIteratorImpl;
 import tdd.iterator.CiruclarListIterator;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
+import java.util.List;
 
 public class CircularListIteratorTest {
+    private CiruclarListIterator list;
+    private List<Integer> elements;
+
+    @BeforeEach
+    void setUp() {
+        this.elements = List.of(10, 20, 30);
+        this.list = new CircularListIteratorImpl(elements);
+    }
 
     @Test
     void testCreation() {
-        final CiruclarListIterator list = new CircularListIteratorImpl(
-            Collections.emptyList()
-            );
+        assertNotNull(this.list);
     }
 
     @Test
     void testEmptyList() {
         final CiruclarListIterator list = new CircularListIteratorImpl(
             Collections.emptyList()
-        );
+            );
         assertTrue(list.isEmpty());
     }
 }
